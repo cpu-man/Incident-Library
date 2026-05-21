05e120bc7e0a1e7fd49787a986db6c6b16f77176
Philip Bryde <brydephilip@gmail.com>
2026-05-21 22:05:00 +0200

    FILES ADDED, WPF WINDOWS ADDED AND C# CLASSES

---
+++ Incident Library/Incident Library/WPF VIEWS/HomePageWindow (Shell).xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Incident_Library.WPF_VIEWS
+{
+    /// <summary>
+    /// Interaction logic for HomePageWindow__Shell_.xaml
+    /// </summary>
+    public partial class HomePageWindow__Shell_ : Window
+    {
+        public HomePageWindow__Shell_()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Incident Library/Incident Library/WPF VIEWS/SUB-VIEWS/UnderReview.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Incident_Library.WPF_VIEWS.SUB_VIEWS
+{
+    /// <summary>
+    /// Interaction logic for UnderReview.xaml
+    /// </summary>
+    public partial class UnderReview : Window
+    {
+        public UnderReview()
+        {
+            InitializeComponent();
+        }
+    }
+}
