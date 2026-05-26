dce78571697502fb99fcf57c37b15c0f1379bee1
Philip Bryde <brydephilip@gmail.com>
2026-05-26 14:12:52 +0200

    All of the Sub Views made + New incident can be created
    A bunch of WPF's pages made for the sub views

--- Incident Library/Incident Library/WPF VIEWS/SUB-VIEWS/AdminView.xaml.cs
+++ Incident Library/Incident Library/WPF VIEWS/SUB-VIEWS/AdminView.xaml.cs
@@ -1,25 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Incident_Library.WPF_VIEWS.SUB_VIEWS
 {
-    /// <summary>
-    /// Interaction logic for AdminView.xaml
-    /// </summary>
-    public partial class AdminView : Window
+    public partial class AdminView : Page
     {
         public AdminView()
         {
             InitializeComponent();
+            // TODO: DataContext = new AdminViewModel();
+            // await ViewModel.LoadUsersAsync();
+        }
+
+        private void UserList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            bool hasSelection = UserList.SelectedItem != null;
+            btnRemoveUser.IsEnabled = hasSelection;
+            btnChangeRole.IsEnabled = hasSelection;
+        }
+
+        private void BtnAddUser_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: open add user dialog
+        }
+
+        private void BtnRemoveUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (UserList.SelectedItem == null) return;
+
+            var result = MessageBox.Show(
+                "Are you sure you want to remove this user?",
+                "Confirm Remove",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Warning);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                // TODO: await ViewModel.RemoveUserAsync(selectedUserId);
+            }
+        }
+
+        private void BtnChangeRole_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: open role picker dialog
         }
     }
 }
--- Incident Library/Incident Library/WPF VIEWS/SUB-VIEWS/UnderReview.xaml.cs
+++ Incident Library/Incident Library/WPF VIEWS/SUB-VIEWS/UnderReview.xaml.cs
@@ -1,25 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace Incident_Library.WPF_VIEWS.SUB_VIEWS
 {
-    /// <summary>
-    /// Interaction logic for UnderReview.xaml
-    /// </summary>
-    public partial class UnderReview : Window
+    public partial class UnderReview : Page
     {
         public UnderReview()
         {
             InitializeComponent();
+            // TODO: DataContext = new IncidentExplorerViewModel();
+            // await ViewModel.LoadIncidentsByStatusAsync(2); // 2 = Under Review
         }
     }
 }
