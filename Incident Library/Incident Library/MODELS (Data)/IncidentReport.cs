5f75907a8024ea5fea4349553b77bd4dfe6edfa8
cpu-man <71815606+cpu-man@users.noreply.github.com>
2026-06-05 00:23:07 +0200

    Read Incident

--- Incident Library/Incident Library/MODELS (Data)/IncidentReport.cs
+++ Incident Library/Incident Library/MODELS (Data)/IncidentReport.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace Incident_Library.MODELS__Data_
 {
-    class IncidentReport
+    public class IncidentReport
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string HowDiscovered { get; set; }
+        public string WhatIsIncident { get; set; }
+        public string HowResolved { get; set; }
+        public int Status { get; set; }
     }
+
+
+
 }
---
+++ Incident Library/Incident Library/Repository/IncidentRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Microsoft.Data.Sqlite;
+using Incident_Library.MODELS__Data_;
+
+namespace Incident_Library.Repository
+{
+    public class IncidentRepository
+    {
+        public List<IncidentReport> Read()
+        {
+            var incidentList = new List<IncidentReport>();
+            using var conncetion = new SqliteConnection("Data Source=IncidentLibrary.db;");
+            conncetion.Open();
+            using var command = new SqliteCommand("SELECT * FROM Incident", conncetion);
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                IncidentReport i = new IncidentReport();
+                i.Id = Convert.ToInt32(reader["IncidentID"]);
+                i.Title = reader["Title"] as string;
+                i.HowDiscovered = reader["HowDiscovered"] as string;
+                i.WhatIsIncident = reader["WhatIsIncident"] as string;
+                i.HowResolved = reader["HowResolved"] as string;
+                i.Status = Convert.ToInt32(reader["StatusID"]);
+
+                incidentList.Add(i);
+            }
+            return incidentList;
+        }
+    }
+}
